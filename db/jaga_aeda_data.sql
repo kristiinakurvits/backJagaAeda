7045f3fa48838e23a9ef20d9cfa7ec2213c2aa84
IndrekT <indrek.turi@gmail.com>
2022-09-08 13:38:31 +0300

    INDREK_BACK - database in WIP

---
+++ db/jaga_aeda_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO public."user" (id, user_name, password) VALUES (DEFAULT, 'kristiina', '123');
+INSERT INTO public."user" (id, user_name, password) VALUES (DEFAULT, 'jaan', '123');
+INSERT INTO public."user" (id, user_name, password) VALUES (DEFAULT, 'indrek', '123');
+
+INSERT INTO public.measure_unit (id, unit) VALUES (DEFAULT, 'kg');
+INSERT INTO public.measure_unit (id, unit) VALUES (DEFAULT, 'g');
+INSERT INTO public.measure_unit (id, unit) VALUES (DEFAULT, 'l');
+INSERT INTO public.measure_unit (id, unit) VALUES (DEFAULT, 'ml');
+INSERT INTO public.measure_unit (id, unit) VALUES (DEFAULT, 'tk');
+
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'Juurviljad');
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'Köögiviljad');
+INSERT INTO public.category (id, name) VALUES (DEFAULT, 'Puuviljad');
+
