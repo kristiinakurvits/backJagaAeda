de5b13c6e2c93efeb873fe9ec403a474bdb5287f
kristiinakurvits <kristiinakurvits@gmail.com>
2022-09-08 11:44:35 +0300

    Initial commit

---
+++ src/main/java/com/teamProject/backJagaAeda/BackJagaAedaApplication.java
@@ -0,0 +1,13 @@
+package com.teamProject.backJagaAeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackJagaAedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackJagaAedaApplication.class, args);
+	}
+
+}
